e8e34447385246c022b760039d9a504d40f83c17
doublev32 <valentin.van32@gmail.com>
2019-04-07 12:42:15 +0800

    Presentation page

---
+++ src/components/Presentation.vue
@@ -0,0 +1,35 @@
+<style lang="scss">
+@import '../assets/css/presentation.scss';
+</style>
+
+<template>
+<div id="header" class="centered">
+  <img src="../assets/header.png"/>
+</div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Presentation extends Vue {
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
