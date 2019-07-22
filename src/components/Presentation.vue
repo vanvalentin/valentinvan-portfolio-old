8636a40f118edadabd7b85dacf1949ab2c84d587
doublev32 <valentin.van32@gmail.com>
2019-07-23 01:32:48 +0800

    Added skill categories+bootstrap

--- src/components/Presentation.vue
+++ src/components/Presentation.vue
@@ -3,8 +3,10 @@
 </style>
 
 <template>
-<div id="header" class="centered">
-  <img src="../assets/header.png"/>
+<div id="header" class="row">
+  <div class="offset-md-3 col-md-6">
+    <img src="../assets/header.png"/>
+  </div>
 </div>
 </template>
 
@@ -15,21 +17,3 @@ import { Component, Prop, Vue } from 'vue-property-decorator';
 export default class Presentation extends Vue {
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
