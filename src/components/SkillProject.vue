840a9006cd29dfe71f5c3aa4abbfd3d8d28f31b4
doublev32 <valentin.van32@gmail.com>
2019-10-20 23:10:15 +0800

    Finishing web page

---
+++ src/components/SkillProject.vue
@@ -0,0 +1,42 @@
+<style lang="scss">
+@import '../assets/css/category-banner.scss';
+</style>
+
+<template>
+<div class="row">
+  <div class="offset-md-2 col-md-4">
+    <img ref="IlluImg" class="illu-img" :src="imgPath"/>
+  </div>
+  <div class="col-md-5">
+    <h1 class="description-title">{{title}}</h1>
+    <p class="description-text" v-html="text"></p>
+  </div>
+</div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class SkillProject extends Vue {
+  @Prop()
+  public imgPath!: string;
+
+  @Prop()
+  public title!: string;
+
+  @Prop()
+  public text!: string;
+
+  public static lastDelay: number = 1;
+
+  constructor() {
+    super();
+  }
+
+  mounted(){
+  }
+}
+
+</script>
+
