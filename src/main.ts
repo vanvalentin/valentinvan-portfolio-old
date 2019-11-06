4682b7b79a357ff3a142043afc5e7fa2cc1270a6
doublev32 <valentin.van32@gmail.com>
2019-11-07 01:29:41 +0800

    remove option analytics

--- src/main.ts
+++ src/main.ts
@@ -6,18 +6,11 @@ import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
 import VueAnalytics from 'vue-analytics';
 
-const isProd = process.env.NODE_ENV === 'production';
-
-
 Vue.config.productionTip = false;
 Vue.use(BootstrapVue);
 Vue.use(VueAnalytics, {
   id: 'UA-151678250-1',
-  router,
-  debug: {
-    enabled: !isProd,
-    sendHitTask: isProd
-  }
+  router
 });
 
 new Vue({
