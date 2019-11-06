5a7d8beaca7acd61bbf859814e6180aa7a512533
doublev32 <valentin.van32@gmail.com>
2019-11-07 01:19:06 +0800

    adding vue-analytics

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,3 @@
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-151678250-1"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'UA-151678250-1');
-</script>
-
 <template>
   <div id="app">
     
--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,14 @@ import router from "./router";
 import BootstrapVue from "bootstrap-vue";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
+import VueAnalytics from 'vue-analytics';
 
 
 Vue.config.productionTip = false;
 Vue.use(BootstrapVue);
+Vue.use(VueAnalytics, {
+  id: 'UA-151678250-1'
+});
 
 new Vue({
   router,
